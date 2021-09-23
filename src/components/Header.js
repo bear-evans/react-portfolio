f9afa1a3d2c93cc4e03b6d748e7da17b2da1685a
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-09-23 17:18:47 -0400

    Basic header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 export default function Header() {
+  const headerStyles = {
+    backgroundImage: `url("./images/header-background.jpg")`,
+    backgroundSize: "cover",
+    height: "80vh",
+    textAlign: "center",
+    paddingTop: "10vh",
+    fontFamily: "Impact, Serif",
+    fontSize: "10em",
+    color: "silver",
+  };
+
   return (
-    <div className="header">
-      <p>Header HERE</p>
+    <div className="bg-dark rounded-lg" style={headerStyles}>
+      <span style={{ paddingRight: "5em" }}>Hi,</span>
+      <br /> I'm <span style={{ color: "white" }}>Bear</span>
     </div>
   );
 }
