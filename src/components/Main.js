5621f809556bbe738d7d186e20c21f60b5542c00
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-09-23 16:05:36 -0400

    Set up blank component pages

---
+++ src/components/Main.js
@@ -0,0 +1,41 @@
+import React, { useState, Fragment } from "react";
+import Navigation from "./Navigation";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
+export default function Main() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <Fragment>
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      <Header />
+      {renderPage}
+      <Footer />
+    </Fragment>
+  );
+}
