d4e0557c6b70ff5cbdc44099831d5c9b5019ea27
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-09-25 10:42:38 -0400

    Added github deploying and contact form dummy

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,18 @@
 import React, { Fragment } from "react";
 
+function handleSubmit() {
+  console.log("This email form is a dummy.");
+}
+
 export default function Contact() {
   return (
     <Fragment>
+      <h2>Contact</h2>
+      <p>Want to get in touch with Bear?</p>
       <script
         src="https://kit.fontawesome.com/ba33a5b68d.js"
         crossorigin="anonymous"
       ></script>
-      <h2>Contact</h2>
-      <p>Want to get in touch with Bear?</p>
       <div class="container d-flex justify-content-around">
         <a
           id="gmail-button"
@@ -41,6 +45,28 @@ export default function Contact() {
           <span> Github </span>
         </a>
       </div>
+      <p className="my-3">Or send Bear a quick email from here.</p>
+      <form id="email-form" onSubmit={handleSubmit} method="POST">
+        <div className="form-group mt-3">
+          <label htmlFor="name-input">Name</label>
+          <input type="text" id="name-input" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email-input">Email address</label>
+          <input type="email" id="email-input" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message-input">Message</label>
+          <textarea
+            id="message-input"
+            className="form-control"
+            rows="5"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </Fragment>
   );
 }
