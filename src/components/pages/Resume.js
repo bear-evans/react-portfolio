f765b188f7df52b109f938ac455ebf65b4cdc246
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-09-24 21:09:02 -0400

    Added resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,19 @@ export default function Resume() {
   return (
     <Fragment>
       <h2>Resume</h2>
-      <p>Resume stuff.</p>
+      <p>My resume is available as a PDF.</p>
+      <div class="container d-flex justify-content-around">
+        <a
+          id="resume-button"
+          class="button"
+          href="https://bear-evans.github.io/portfolio/assets/Resume.pdf"
+        >
+          <span class="icon">
+            <i class="far fa-file-pdf"></i>
+          </span>
+          <span> Resume </span>
+        </a>
+      </div>
     </Fragment>
   );
 }
