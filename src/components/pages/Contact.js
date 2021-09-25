b7dbb3effe81ff269c13470a32bb1e2c7ee5fd97
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-09-24 21:00:34 -0400

    Added contact buttons

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,8 +3,44 @@ import React, { Fragment } from "react";
 export default function Contact() {
   return (
     <Fragment>
+      <script
+        src="https://kit.fontawesome.com/ba33a5b68d.js"
+        crossorigin="anonymous"
+      ></script>
       <h2>Contact</h2>
-      <p>Contact stuff.</p>
+      <p>Want to get in touch with Bear?</p>
+      <div class="container d-flex justify-content-around">
+        <a
+          id="gmail-button"
+          class="btn btn-danger"
+          href="mailto:bear-evans@outlook.com"
+        >
+          <span class="icon">
+            <i class="far fa-envelope"></i>
+          </span>
+          <span> Email </span>
+        </a>
+        <a
+          id="linkedin-button"
+          class="btn btn-primary"
+          href="http://www.linkedin.com/in/bear-evans"
+        >
+          <span class="icon">
+            <i class="fab fa-linkedin"></i>
+          </span>
+          <span> LinkedIn </span>
+        </a>
+        <a
+          id="github-button"
+          class="btn btn-success"
+          href="https://github.com/bear-evans/"
+        >
+          <span class="icon">
+            <i class="fab fa-github"></i>
+          </span>
+          <span> Github </span>
+        </a>
+      </div>
     </Fragment>
   );
 }
